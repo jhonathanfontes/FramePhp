6daa30409dfb851f4dfd0eff8575911c2e7cbeba
Jhonathan Fontes <jhonathanfontes@gmail.com>
2025-05-27 15:00:08 -0300

    Bugado

--- public/index.php
+++ public/index.php
@@ -1,27 +1,34 @@
 <?php
 
-// Definir caminho base
+// Define base path
 define('BASE_PATH', dirname(__DIR__));
 
-// Carregar bootstrap
-require_once BASE_PATH . '/bootstrap/app.php';
-
-
-// Carregar o autoloader do Composer
+// Load Composer's autoloader
 require BASE_PATH . '/vendor/autoload.php';
 
-// Carregar variáveis de ambiente
-$dotenv = \Dotenv\Dotenv::createImmutable(BASE_PATH);
-$dotenv->load();
+// Load environment variables
+try {
+    if (!file_exists(BASE_PATH . '/.env')) {
+        if (file_exists(BASE_PATH . '/.env.example')) {
+            copy(BASE_PATH . '/.env.example', BASE_PATH . '/.env');
+            echo "Arquivo .env foi criado automaticamente a partir do .env.example. Por favor, configure as variáveis de ambiente apropriadamente.\n";
+        } else {
+            throw new Exception("Arquivo .env.example não encontrado. Por favor, crie o arquivo .env manualmente.");
+        }
+    }
+    
+    $dotenv = \Dotenv\Dotenv::createImmutable(BASE_PATH);
+    $dotenv->load();
 
-// Carregar bootstrap
+} catch (Exception $e) {
+    die("Erro ao carregar variáveis de ambiente: " . $e->getMessage());
+}
+
+use Core\Config\Constants;
+Constants::init();
+
+// Registrar middlewares no container
+$container = \Core\Container\Container::getInstance();
+
+// Load bootstrap
 require_once BASE_PATH . '/bootstrap/app.php';
-
-// Inicializar o Router
-$router = new \Core\Router\Router();
-
-// Carregar as rotas
-require_once BASE_PATH . '/routes/web.php';
-
-// Despachar a rota
-$router->dispatch();
